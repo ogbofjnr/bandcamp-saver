4dcd369f49d07375bf84c217999f083a750f3cee
morow93 <derchepur@gmail.com>
2016-03-09 03:07:46 +0300

    now you can download 'head' track too

--- js/content.js
+++ js/content.js
@@ -1,6 +1,13 @@
-console.log("BandCampSaver started");
+BandcampSaver = (function(){
 
-BandCampSaver = (function(){
+    String.format = function() {
+          var s = arguments[0];
+          for (var i = 0; i < arguments.length - 1; i++) {
+              var reg = new RegExp("\\{" + i + "\\}", "gm");
+              s = s.replace(reg, arguments[i + 1]);
+          }
+          return s;
+    };
 
     var CONSTANTS = {
         ERRORS : {
@@ -10,59 +17,61 @@ BandCampSaver = (function(){
             DOWNLOAD: "Error while sound download"
         },
         SELECTORS: {
-            ALBUM: "[itemtype='http://schema.org/MusicAlbum']",//.trackView or #trackInfo
-            SOUND: "[itemtype='http://www.schema.org/MusicRecording']",//tr or .track_row_view
+            ALBUM: ".trackView",//[itemtype='http://schema.org/MusicAlbum'] or #trackInfo
+            SOUNDS: "[itemtype='http://www.schema.org/MusicRecording']",//tr or .track_row_view
+            SOUND: "table:nth-child(1) tr:nth-child(1) td:nth-child(2) a",
             SAVE_LNK: "bcs_save-lnk",
-            TRACK_OBJECT: "TralbumData"
+            SAVE_BTN: "bcs_save-btn",
+            SOUND_OBJECT: "TralbumData"
         },
         HTML: {
-            SAVE_LNK_TEMPLATE: "<a class='{0}' data-page='{1}' href='javascript:void(0)'>download</a>"
+            SAVE_LNK_TEMPLATE: "<a class='{0}' data-page='{1}' href='javascript:void(0)'>download</a>",
+            SAVE_BTN_TEMPLATE: "<h4><a class='bcs_save-btn' href='javascript:void(0)'>Download Now</a></h4>"
         }
     };
 
     var settings = {
-        url: window.location.origin,
-        protocol: window.location.protocol
+        url: window.location.origin
     };
 
     return {
         loading: false,
         init: function() {
-            if (BandCampSaver.preparePage()){
-                BandCampSaver.bindUIActions();
+            if (BandcampSaver.preparePage()){
+                BandcampSaver.bindUIActions();
             };
         },
         preparePage: function(){
-            var wrapper, sounds;
+            var sounds;
             try{
-                wrapper = $(CONSTANTS.SELECTORS.ALBUM);
-                sounds = $(wrapper).find(CONSTANTS.SELECTORS.SOUND);
+                sounds = $($(CONSTANTS.SELECTORS.ALBUM)).find(CONSTANTS.SELECTORS.SOUNDS);
             }catch(e){
                 console.log(CONSTANTS.ERRORS.SELECT_DOM);
                 return false;
             }
             try{
                 $(sounds).each(function(){
-                    var soundPage, soundTitle = $(this).find("td")[2];
-                    $(soundTitle).find("a").each(function() {
+                    var soundPage, soundInfo = $(this).find("td")[2];
+                    $(soundInfo).find("a").each(function() {
                         soundPage = $(this).attr("href");
                         if (soundPage) return false;
                     });
                     var lnk = String.format(CONSTANTS.HTML.SAVE_LNK_TEMPLATE, CONSTANTS.SELECTORS.SAVE_LNK, soundPage);
-                    $(soundTitle).children().append(lnk);
+                    $(soundInfo).children().append(lnk);
                 });
+                $($(".buyItem").children("h4")[0]).after(CONSTANTS.HTML.SAVE_BTN_TEMPLATE);
             }catch(e){
                 console.log(CONSTANTS.ERRORS.UPDATE_DOM);
                 return false;
             }
             return true;
         },
-        download: function(elem){
+        download: function(elem, forcibly){
             //get sound page
-            BandCampSaver.loading = true;
+            BandcampSaver.loading = true;
             $.get(settings.url + $(elem).data("page"), function(data) {
                 try{
-                    var index = data.indexOf(CONSTANTS.SELECTORS.TRACK_OBJECT);
+                    var index = data.indexOf(CONSTANTS.SELECTORS.SOUND_OBJECT);
                     if (index >= 0){
                         var tempData = '';
                         for(var i = index; data[i] != ';'; ++i){
@@ -70,27 +79,43 @@ BandCampSaver = (function(){
                         }
                         var evalData = eval(tempData);
                         var soundUrl = evalData.trackinfo[0].file["mp3-128"];
-                        //for get sound properly
+                        //get sound properly
                         var trackId = Math.random().toString(36).substring(7);
-                        $(elem).attr("href", soundUrl);
-                        $(elem).attr('download','');
                         $(elem).attr("id", trackId);
+                        $(elem).attr("download", "");
+                        $(elem).attr("href", soundUrl);
+                        //downloading is here
                         document.getElementById(trackId).click();
+                        //clear href data
+                        if (forcibly){
+                            $(elem).removeAttr("id")
+                            $(elem).removeAttr("download");
+                            $(elem).attr("href", "javascript:void(0)");
+                        }
                     }
                 }catch(e){
                     console.log(CONSTANTS.ERRORS.DOWNLOAD);
-                    BandCampSaver.loading = false;
+                    BandcampSaver.loading = false;
                 }
             }).fail(function() {
                 console.log(CONSTANTS.ERRORS.REQUEST);
             }).always(function() {
-                BandCampSaver.loading = false;
+                setTimeout(function() {
+                    BandcampSaver.loading = false;
+                }, 150);
             });
         },
         bindUIActions: function() {
             $(document).on("click", "." + CONSTANTS.SELECTORS.SAVE_LNK, function(event) {
-                if (!event.target.hasAttribute("download") && !BandCampSaver.loading){
-                    BandCampSaver.download($(event.target));
+                if (!event.target.hasAttribute("download") && !BandcampSaver.loading){
+                    BandcampSaver.download($(event.target));
+                }
+            });
+            $(document).on("click", "." + CONSTANTS.SELECTORS.SAVE_BTN, function(event) {
+                if (!BandcampSaver.loading){
+                    var sound = $($(CONSTANTS.SELECTORS.ALBUM)).find(CONSTANTS.SELECTORS.SOUND);
+                    $(event.target).data("page", sound.attr("href"));
+                    BandcampSaver.download($(event.target), true);
                 }
             });
         }
@@ -99,14 +124,5 @@ BandCampSaver = (function(){
 })();
 
 $(document).ready(function() {
-    BandCampSaver.init();
+    BandcampSaver.init();
 });
-
-String.format = function() {
-      var s = arguments[0];
-      for (var i = 0; i < arguments.length - 1; i++) {
-          var reg = new RegExp("\\{" + i + "\\}", "gm");
-          s = s.replace(reg, arguments[i + 1]);
-      }
-      return s;
-};
